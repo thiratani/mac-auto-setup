e1f162cd85cb37a0c866aee762953a4c86348e82
tkylab <takuya.hiratani.39@gmail.com>
2020-07-27 13:54:29 +0900

    Removed unnecessary code and files.

--- appstore.sh
+++ appstore.sh
@@ -11,7 +11,6 @@ EOS
 # Mac App Store apps install
 #
 echo " ---- Mac App Store apps -----"
-brew install mas
 mas install 409183694  # Keynote
 mas install 409201541  # Pages 
 mas install 409203825  # Numbers
