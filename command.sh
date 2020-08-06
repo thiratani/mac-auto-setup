35cf5663fa02d7dcf23aad51332ca75c1e38f2e1
tkylab <takuya.hiratani.39@gmail.com>
2020-08-06 09:04:45 +0900

    Added a script for command installation.

---
+++ command.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+cat << EOS
+
+ TkyLab
+
+ me, and the other.
+
+EOS
+
+#
+# Install command.
+#
+echo " ----- Install command ------"
+brew install ag 
+npm -g install gulp
+
+while true; do
+  read -p 'Add Other command "For Private"? [Y/n]' Answer
+  case $Answer in
+    '' | [Yy]* )
+      break;
+      ;;
+    [Nn]* )
+      echo "Skip install"
+      break;
+      ;;
+    * )
+      echo Please answer YES or NO.
+  esac
+done;
+echo " ------------ END ------------"
+
